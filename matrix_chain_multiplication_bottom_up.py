c89b42da9fafeb81113110e10782aa8cc3016db6
Anantvir Singh <anantvir91@gmail.com>
2019-08-07 23:29:16 -0400

    Matrix Chain Multiplication using bottom up dynamic programming

--- matrix_chain_multiplication_bottom_up.py
+++ matrix_chain_multiplication_bottom_up.py
@@ -1,7 +1,7 @@
 """Author - Anantvir Singh, concept reference:= CLRS, Dynamic Programming page 370"""
 
 #---------------------------Bottom-up Approach-----------------------------------
-
+import math
 def Matrix_Chain_Multiplication(p):
     n = len(p) - 1
     m = [[0 for x in range(n+1)] for x in range(n+1)]       # To keep the program simpler m[0][x] row and m[x][0] column are unused 
@@ -12,7 +12,7 @@ def Matrix_Chain_Multiplication(p):
     for l in range(2,n+1):                      # Length of chain to be considered
         for i in range(1,n-l+2):
             j = i + l -1                        # i and j are calculated according to length of chain considered. Example for l=3, if i=1 then j=3 which represents multiplication of matrices A1,A2,A3,if i=2 then j=4 which is A2*A3*A4,if i=3 then j=5 which is A3*A4*A5 and so on.. 
-            m[i][j] = 10000000000000            # +ve infinity
+            m[i][j] = math.inf            # +ve infinity
             for k in range(i,j):
                 q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j]      # Optimal sub structure
                 if q < m[i][j]:
