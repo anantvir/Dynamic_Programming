94104443d770430aec85dcfd9a89591f377eb5ae
Anantvir Singh <anantvir91@gmail.com>
2019-08-07 23:29:51 -0400

    Matrix Chain Multiplications using Memoization

--- matrix_chain_multiplication_memoized.py
+++ matrix_chain_multiplication_memoized.py
@@ -1,17 +1,17 @@
 """Author - Anantvir Singh, concept reference:= CLRS, Dynamic Programming page 370"""
 
 #---------------------------Memoization Approach-----------------------------------
-
+import math
 def Matrix_Chain_Memoized(p):
     n = len(p) - 1
     m = [[0 for x in range(n+1)]for x in range(n+1)]
     for i in range(1,n+1):
         for j in range(1,n+1):
-            m[i][j] = 10000000000
+            m[i][j] = math.inf
     return Matrix_Chain_Lookup(p,m,1,n)
 
 def Matrix_Chain_Lookup(p,m,i,j):
-    if m[i][j] < 10000000000:
+    if m[i][j] < math.inf:
         return m[i][j]
     if i == j:
         m[i][j] = 0             # no cost of multiplying single matrix with itself
